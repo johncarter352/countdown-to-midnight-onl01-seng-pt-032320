7ee67f9d9303981106707fd5275fbd21bce8f8f0
John Carter <johncarter352@gmail.com>
2020-03-29 20:56:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
-#write your code here
-
-def countdown
+def countdown(number)
+  number = 5
+  while number < 0
+  puts "{number} SECOND(S)"
+  countdown -= 1
+  while number = 0
+  puts "HAPPY NEW YEAR"
 end
+end
+
